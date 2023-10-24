880cf9d14edd2ce626d95abe1af3fb287d921cbb
Mishima <mpowershinto@gmail.com>
2023-10-24 17:26:46 +0900

    second commit

---
+++ Core/App/AA.h
@@ -0,0 +1,17 @@
+/*
+ * AA.h
+ *
+ *  Created on: Oct 24, 2023
+ *      Author: G4T1PR0
+ */
+
+#ifndef APP_AA_H_
+#define APP_AA_H_
+
+class AA {
+public:
+	AA();
+	virtual ~AA();
+};
+
+#endif /* APP_AA_H_ */
---
+++ Core/App/app_main.cpp
@@ -0,0 +1,17 @@
+/*
+ * app_main.c
+ *
+ *  Created on: Oct 24, 2023
+ *      Author: G4T1PR0
+ */
+
+#include "app_main.h"
+#include "AA.h"
+
+AA a;
+
+void app_main(void) {
+	while(1) {
+
+	}
+}
